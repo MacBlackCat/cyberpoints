2557c8d6ef077596a16271b04d1a4da0275de382
Yankı Küçük <yankikucuk@Yank-iMac.local>
2018-08-28 23:54:10 +0300

    utilities

--- test/tester.js
+++
@@ -1,8 +0,0 @@
-const {
-  Blizzard: { Wow }
-} = require('../src/main');
-const config = require('./config-test');
-const wow = new Wow(config);
-
-// eslint-disable-next-line
-console.log(wow.community.character.profile())
